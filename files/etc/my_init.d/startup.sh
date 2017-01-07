b24261e76174b1c12b4b98fc6f4749190cd16553
Tom Noogen <tom@noogen.net>
2017-01-06 18:46:59 -0600

    Update startup.sh

--- files/etc/my_init.d/startup.sh
+++ files/etc/my_init.d/startup.sh
@@ -11,9 +11,11 @@ then
     sed -i -e "s/\%ip\%\:\%proxy\_port\%\;/\%proxy\_port\%\;/g" /vesta/local/vesta/data/templates/web/nginx/default.tpl
     sed -i -e "s/\%ip\%\:\%proxy\_port\%\;/\%proxy\_port\%\;/g" /vesta/local/vesta/data/templates/web/nginx/default.stpl
     
-    sed -i -e "s/proxy_set_header X-Real-IP \$remote_addr\;\n      proxy_pass/proxy_pass/g" /vesta/local/vesta/data/templates/web/nginx/default.tpl
-    sed -i -e "s/proxy_set_header X-Real-IP \$remote_addr\;\n      proxy_pass/proxy_pass/g" /vesta/local/vesta/data/templates/web/nginx/default.stpl
-
+    # sed -i -e "s/proxy_set_header X-Real-IP \$remote_addr\;\n      proxy_pass/proxy_pass/g" /vesta/local/vesta/data/templates/web/nginx/default.tpl
+    # sed -i -e "s/proxy_set_header X-Real-IP \$remote_addr\;\n      proxy_pass/proxy_pass/g" /vesta/local/vesta/data/templates/web/nginx/default.stpl
+    
+    bash /usr/local/vesta/upd/switch_rpath.sh
+    
     # disable localhost redirect to bad default IP
     sed -i -e "s/^NAT=.*/NAT=\'\'/g" /vesta/local/vesta/data/ips/127.0.0.1
 
