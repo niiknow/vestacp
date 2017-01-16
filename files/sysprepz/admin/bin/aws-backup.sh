80bcaa2040b3f2613e06545eede6c4aaf851cbfe
Tom Noogen <tom@noogen.net>
2017-01-16 17:52:48 -0600

    Update aws-backup.sh

--- files/sysprepz/admin/bin/aws-backup.sh
+++ files/sysprepz/admin/bin/aws-backup.sh
@@ -5,5 +5,6 @@ export PATH
 backup_dest=$(sed 's/\/*$//' <<< "$1")
 backup_dest=$(sed 's/^\/*//' <<< "$backup_dest")
 
-aws s3 sync "/backup/" "s3://$backup_dest/' --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
+# this assume that you already ssh in as admin and run cli command "aws configure" to save your credential
+aws s3 sync "/backup/" "s3://$backup_dest/" --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
 find /home/admin/tmp -type f -mtime +30 -exec rm -f {} \;
