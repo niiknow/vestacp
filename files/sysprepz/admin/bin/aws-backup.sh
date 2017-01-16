7c2fb6aebe614d5241cd8538b3e0a15fc5270412
Tom Noogen <tom@tnguyen-imac.local>
2017-01-16 12:46:18 -0600

    update backup script

--- files/sysprepz/admin/bin/aws-backup.sh
+++ files/sysprepz/admin/bin/aws-backup.sh
@@ -2,6 +2,8 @@
 # sync to s3, log to tmp, cleanup tmp
 PATH="/home/admin/bin:/home/admin/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
 export PATH
+backup_dest=$(sed 's/\/*$//' <<< "$1")
+backup_dest=$(sed 's/^\/*//' <<< "$backup_dest")
 
-aws s3 sync '/backup/' 's3://brick-backups/gogs201701/' --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
+aws s3 sync "/backup/" "s3://$backup_dest/' --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
 find /home/admin/tmp -type f -mtime +30 -exec rm -f {} \;
