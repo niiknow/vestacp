4a5a9e9b005e64430033af18ea73efe398f2142d
Tom Noogen <tom@tnguyen-imac.local>
2017-06-22 12:05:55 -0500

    improve backup sorting

--- files/sysprepz/admin/bin/aws-backup.sh
+++ files/sysprepz/admin/bin/aws-backup.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # sync to s3, log to tmp, cleanup tmp
 PATH="/home/admin/bin:/home/admin/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
 export PATH
 backup_dest=$(sed 's/\/*$//' <<< "$1")
 backup_dest=$(sed 's/^\/*//' <<< "$backup_dest")
+backup_sort=$(expr 9999999999 - $(date +%Y%m%d%H))
 
 # this assume that you already ssh in as admin and run cli command "aws configure" to save your credential
-aws s3 sync "/backup/" "s3://$backup_dest/" --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
+aws s3 sync "/backup/" "s3://$backup_dest/$backup_sort/" --storage-class REDUCED_REDUNDANCY --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
 find /home/admin/tmp -type f -mtime +30 -exec rm -f {} \;
