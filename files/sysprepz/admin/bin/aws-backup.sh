4db497b7c768b0ffb26403a6f957732d8aeeeb3c
Tom Noogen <tom@tnguyen-imac.local>
2017-06-23 09:47:47 -0500

    sort by month

--- files/sysprepz/admin/bin/aws-backup.sh
+++ files/sysprepz/admin/bin/aws-backup.sh
@@ -4,8 +4,10 @@ PATH="/home/admin/bin:/home/admin/.local/bin:/usr/local/sbin:/usr/local/bin:/usr
 export PATH
 backup_dest=$(sed 's/\/*$//' <<< "$1")
 backup_dest=$(sed 's/^\/*//' <<< "$backup_dest")
-backup_sort=$(expr 9999999999 - $(date +%Y%m%d%H))
+backup_date=$(date +%Y%m)
+backup_date2=$(expr 999999 - $(date +%Y%m))
+backup_sort=$backup_date2"_"$backup_date
 
 # this assume that you already ssh in as admin and run cli command "aws configure" to save your credential
-aws s3 sync "/backup/" "s3://$backup_dest/" --storage-class REDUCED_REDUNDANCY --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
+aws s3 sync "/backup/" "s3://$backup_dest/$backup_sort/" --storage-class REDUCED_REDUNDANCY --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
 find /home/admin/tmp -type f -mtime +30 -exec rm -f {} \;
