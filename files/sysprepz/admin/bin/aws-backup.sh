3b02600113a61d6209a35cff05f5b45a16af3f61
Tom Noogen <tom@noogen.net>
2017-07-24 21:38:04 -0500

    AWS sync IA is actually the lower price, RR is higher and being phased out

--- files/sysprepz/admin/bin/aws-backup.sh
+++ files/sysprepz/admin/bin/aws-backup.sh
@@ -9,5 +9,5 @@ backup_date2=$(expr 999999 - $(date +%Y%m))
 backup_sort=$backup_date2"_"$backup_date
 
 # this assume that you already ssh in as admin and run cli command "aws configure" to save your credential
-aws s3 sync "/backup/" "s3://$backup_dest/$backup_sort/" --storage-class REDUCED_REDUNDANCY --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
+aws s3 sync "/backup/" "s3://$backup_dest/$backup_sort/" --storage-class STANDARD_IA --exclude="*" --include "*.tar" >> /home/admin/tmp/aws-backup."$(date +%F_%R)".log
 find /home/admin/tmp -type f -mtime +30 -exec rm -f {} \;
