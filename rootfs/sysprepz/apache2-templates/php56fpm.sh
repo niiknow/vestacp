4fa226ccec1d26c4a2026f1a5f32c9637390e878
Tom Noogen <tom@noogen.net>
2017-11-10 21:42:42 -0600

    more adjustment

--- rootfs/sysprepz/apache2-templates/php56fpm.sh
+++ rootfs/sysprepz/apache2-templates/php56fpm.sh
@@ -13,7 +13,7 @@ user = $user
 group = www-data
 
 listen = /var/run/vesta-php-fpm-$domain.sock
-listen.owner = admin
+listen.owner = $user
 listen.group = www-data
 listen.mode = 0660
 
@@ -28,8 +28,9 @@ fpm_conf_file="$home_dir/$user/web/$domain/cgi-bin/php-fpm.conf"
 echo "$fpm_conf" > $fpm_conf_file
 chown $user:$user $fpm_conf_file
 chmod -f 751 $fpm_conf_file
+rm -rf $home_dir/$user/web/$domain/tmp/cache
 mkdir -p $home_dir/$user/web/$domain/tmp/cache
-chown -R $user:www-data $home_dir/$user/web/$domain/tmp
+chown -R www-data:www-data $home_dir/$user/web/$domain/tmp/cache
 
 # delete old and link new conf
 rm -f /etc/php/*/fpm/pool.d/$domain.conf
@@ -39,6 +40,9 @@ ln -sf $fpm_conf_file /etc/php/$php_version/fpm/pool.d/$domain.conf
 service php$php_version-fpm start || true
 
 # restart if it's already running
-service php$php_version-fpm restart || true
+service php5.6-fpm restart || true
+service php7.0-fpm restart || true
+service php7.1-fpm restart || true
+service nginx restart || true
 
 exit 0
