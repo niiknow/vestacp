77876e196c6a875e3c10c5a910379570d6541318
Tom Noogen <tom@noogen.net>
2017-12-01 10:02:39 -0600

    update startup

--- rootfs/etc/my_init.d/startup.sh
+++ rootfs/etc/my_init.d/startup.sh
@@ -4,8 +4,9 @@ export TERM=xterm
 
 if [ -z "`ls /vesta/etc --hide='lost+found'`" ]
 then
-    rsync -a /vesta-start/* /vesta
-    rsync -a /sysprepz/home/* /home
+    echo "[i] running for the 1st time"
+    rsync --update -raz --progress /vesta-start/* /vesta
+    rsync --update -raz --progress /sysprepz/home/* /home
 
 # save some bytes, you can do it later
 #    rm -rf /sysprepz
@@ -13,7 +14,8 @@ then
 fi
 
 # restore current users
-if [[ -f /backup/.etc/passwd ]]; then
+if [ -f /backup/.etc/passwd ]; then
+    echo "[i] restoring existing users"
 	# restore users
 	rsync -a /backup/.etc/passwd /etc/passwd
 	rsync -a /backup/.etc/shadow /etc/shadow
@@ -28,11 +30,13 @@ chown www-data:www-data /var/ngx_pagespeed_cache
 chmod 750 /var/ngx_pagespeed_cache
 
 if [ -f /etc/nginx/nginx.new ]; then
+    echo "[i] init nginx"
 	mv /etc/nginx/nginx.conf /etc/nginx/nginx.old
 	mv /etc/nginx/nginx.new /etc/nginx/nginx.conf
 fi
 
 if [[ -f /etc/fail2ban/jail.new ]]; then
+    echo "[i] init fail2ban"
     mv /etc/fail2ban/jail.local /etc/fail2ban/jail-local.bak
     mv /etc/fail2ban/jail.new /etc/fail2ban/jail.local
 fi
