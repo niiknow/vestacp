9c92fc8b4ba8d20a9c7cc76435419c0867c1fb7a
noogen <tom@noogen.net>
2019-05-17 11:09:15 -0500

    fix openbase dir

--- rootfs/sysprepz/apache2-templates/php73fpm.sh
+++ rootfs/sysprepz/apache2-templates/php73fpm.sh
@@ -5,7 +5,7 @@ domain="$2"
 ip="$3"
 home_dir="$4"
 docroot="$5"
-php_version="7.3"
+php_version="7.2"
 
 fpm_conf="
 [$domain]
@@ -24,9 +24,9 @@ pm.max_requests = 4000
 pm.process_idle_timeout = 10s
 pm.status_path = /status
 
-php_admin_value[upload_tmp_dir] = /home/$user/tmp
-php_admin_value[session.save_path] = /home/$user/tmp
-php_admin_value[open_basedir] = $docroot:/home/$user/tmp
+php_admin_value[upload_tmp_dir] = /home/$user/web/$domain/tmp
+php_admin_value[session.save_path] = /home/$user/web/$domain/tmp
+php_admin_value[open_basedir] = $docroot:/home/$user/web/$domain/tmp
 
 env[HOSTNAME] = $HOSTNAME
 env[PATH] = /usr/local/bin:/usr/bin:/bin
@@ -34,10 +34,10 @@ env[TMP] = /home/$user/tmp
 env[TMPDIR] = /home/$user/tmp
 env[TEMP] = /home/$user/tmp
 "
-fpm_conf_file="$home_dir/$user/web/$domain/cgi-bin/php-fpm.conf"
+fpm_conf_file="/home/$user/web/$domain/cgi-bin/php-fpm.conf"
 
 # remove old conf
-rm -f $home_dir/$user/web/$domain/cgi-bin/php*-fpm.conf
+rm -f /home/$user/web/$domain/cgi-bin/php*-fpm.conf
 
 # restart any *running* php fpm found with ps -uaxw
 # otherwise, simply use: 
@@ -53,14 +53,16 @@ do
   fi
 done
 
+# make sure to delete old sock file before restart
 rm -f /var/run/vesta-php-fpm-$domain.sock || true
 
 echo "$fpm_conf" > $fpm_conf_file
 chown $user:$user $fpm_conf_file
 chmod -f 751 $fpm_conf_file
-rm -rf $home_dir/$user/web/$domain/tmp/cache
-mkdir -p $home_dir/$user/web/$domain/tmp/cache
-chown -R www-data:www-data $home_dir/$user/web/$domain/tmp/cache
+mkdir -p /home/$user/web/$domain/tmp/cache
+rm -rf /home/$user/web/$domain/tmp/cache
+mkdir -p /home/$user/web/$domain/tmp/cache
+chown -R www-data:www-data /home/$user/web/$domain/tmp/cache
 
 # delete old and link new conf
 rm -f /etc/php/*/fpm/pool.d/$domain.conf
